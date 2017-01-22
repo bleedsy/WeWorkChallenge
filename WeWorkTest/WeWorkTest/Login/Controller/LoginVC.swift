24f58d0414093fc844424f777960e81c2c996f15
Brandon Leeds <Brandon@Brandons-MacBook-Air.local>
2017-01-22 18:39:04 -0500

    added LoginVC

---
+++ WeWorkTest/WeWorkTest/Login/Controller/LoginVC.swift
@@ -0,0 +1,31 @@
+//
+//  LoginVC.swift
+//  WeWorkTest
+//
+//  Created by Brandon Leeds on 1/22/17.
+//  Copyright © 2017 Brandon Leeds. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    @IBOutlet weak var IBusername : UITextField!
+    @IBOutlet weak var IBpassword : UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func login(sender: UIButton) {
+        
+    }
+}
