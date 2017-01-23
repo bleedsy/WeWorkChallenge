2481ac2666c3c2daaa6a1e3faba7a30385bcf304
Brandon Leeds <Brandon@Brandons-MacBook-Air.local>
2017-01-22 22:37:11 -0500

    Bridging header file

---
+++ WeWorkTest/WeWorkTest/Profile/Controller/ProfileVC.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileVC.swift
+//  WeWorkTest
+//
+//  Created by Brandon Leeds on 1/22/17.
+//  Copyright © 2017 Brandon Leeds. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: RootVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ WeWorkTest/WeWorkTest/WeWorkTest-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  WeWorkTest-Bridging-Header.h
+//  WeWorkTest
+//
+//  Created by Brandon Leeds on 1/22/17.
+//  Copyright © 2017 Brandon Leeds. All rights reserved.
+//
+
+#import <MBProgressHUD/MBProgressHUD.h>
