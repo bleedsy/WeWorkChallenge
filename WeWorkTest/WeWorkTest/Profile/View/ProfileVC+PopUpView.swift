d0ee5ccba0c87dd9f7bbb7dad36e7a70a3c8e983
Brandon Leeds <Brandon@Brandons-MacBook-Air.local>
2017-01-27 11:53:29 -0500

    Comments

--- WeWorkTest/WeWorkTest/Profile/View/ProfileVC+PopUpView.swift
+++ WeWorkTest/WeWorkTest/Profile/View/ProfileVC+PopUpView.swift
@@ -10,25 +10,32 @@ import UIKit
 
 extension ProfileVC {
 
+    // Add Nav controller and dim overlay
     func showIssues(repo: RepoObject) {
+        // Create overlay and add as subview
         welDimOverlay = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
         welDimOverlay.backgroundColor = UIColor(colorLiteralRed: 0, green: 0, blue: 0, alpha: 0.50)
         view.addSubview(welDimOverlay)
         
+        // Get nav controller from storyboard
         welIssuesNavController = getVCFromStoryboard("Main", viewController: "IssuesNavController") as! UINavigationController
+        // Setup view controller
         welIssuesController = welIssuesNavController.viewControllers.first as! IssuesVC
         welIssuesController.welDelegate = self
         welIssuesController.welRepo = repo
+        // Setup nav controller
         welIssuesNavController.view.frame = CGRect(x: 0, y: 0, width: 740, height: 500)
         welIssuesNavController.view.center = view.center
         welIssuesNavController.view.layer.cornerRadius = 5
         welIssuesNavController.view.layer.masksToBounds = true
+        // Add nav controller
         addChildViewController(welIssuesNavController)
         view.addSubview(welIssuesNavController.view)
         welIssuesNavController.didMove(toParentViewController: self)
     }
     
     //MARK: Issues Delegate
+    // Remove dim overlay and nav controller from view controller
     func remove() {
         welIssuesNavController.view.removeFromSuperview()
         welIssuesNavController.removeFromParentViewController()
